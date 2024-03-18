43515651773cc7daf6ce2132e19db73bb4f568bd
Zairaxx <brandontsegai@gmail.com>
2024-03-18 10:20:46 +0100

    added comments

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ data.push({
   name: "Wonder woman",
   age: 39,
 });
-// spread-operator
+// spread-operator - skapa kopia av array för att inte mutera originaldata
 let sortedData = [...data];
 
 sortedData.sort((a, b) => a.age - b.age);
 
-// console.log(data);
 // Non-mutating array method - filter
 
 let filteredArray = data.filter((obj) => obj.age > 40);
 
 // console.log(filteredArray);
 
+//Funktion för att sortera en array baserat på valfri property med [] istället för .
 let sortArray = (arr, property) => {
   let newArray = [...arr];
   newArray.sort((a, b) => a[property] - b[property]);
