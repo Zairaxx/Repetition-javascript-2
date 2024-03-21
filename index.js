4233d0c8c9e50187a6d22d1292d132243f491cdb
Zairaxx <brandontsegai@gmail.com>
2024-03-21 10:59:54 +0100

    prototypes

--- index.js
+++ index.js
@@ -1,99 +1,51 @@
-console.log("test");
+//Extends-keywordet - Prototypkedjor
 
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-  getTitle() {
-    return "Titel: " + this.title;
-  }
-  getAuthor() {
-    return "Författare: " + this.author;
-  }
-}
-
-let book1 = new Book("Harry Potter", "J.K Rowling");
-
-console.log(book1.getTitle());
-
-class Employee {
+class Person {
   constructor(firstName, lastName) {
-    // this.firstName = firstName
-    // this.lastName = lastName
-    this.fullName = firstName + " " + lastName;
-    this.fullName = {
-      firstName: firstName,
-      lastName: lastName,
-    };
-    this.email = `${firstName}.${lastName}@company.se`.toLowerCase();
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greeting() {
+    console.log("Hi my name is " + this.firstName + " " + this.lastName + "!");
+  }
+}
+class Teacher extends Person {
+  constructor(firstName, lastName, subjects, students = 0) {
+    super(firstName, lastName);
+    this.amountOfStudent = students;
+    this.subjects = subjects;
+    this.studentsGraded = 0;
+  }
+
+  gradeStudent(student, grade) {
+    student.grade = grade;
+    this.studentsGraded++;
   }
 }
 
-let newEmployee = new Employee("John", "Smith");
-console.log(newEmployee);
-
-//Kalkylator
-
-class Calculator {
-  add(a, b) {
-    return a + b;
-  }
-  subtract(a, b) {
-    return a - b;
-  }
-  multiply(a, b) {
-    return a * b;
-  }
-  divide(a, b) {
-    return a / b;
+class Student extends Person {
+  constructor(firstName, lastName, grade) {
+    super(firstName, lastName);
+    this.grade = grade;
   }
 }
 
-let calculator = new Calculator();
-
-console.log(calculator.divide(10, 5));
-
-//Glass
-
-class IceCream {
-  constructor(flavor, numOfSprinkles) {
-    this.flavor = flavor;
-    this.numOfSprinkles = numOfSprinkles;
+class SubstiteTeacher extends Teacher {
+  constructor(firstName, lastName, subjects) {
+    super(firstName, lastName, subjects);
   }
 }
 
-let chocoIceCream = new IceCream("Chocolate", 7);
-let vanillaIceCream = new IceCream("Vanilla", 16);
-let strawberryIceCream = new IceCream("Strawberry", 2);
+let t1 = new Teacher("Brandon", "Tsegai", 70, ["Frontend", "UX", "Backend"]);
+// console.log(t1.constructor.name);
 
-let calculateSweetness = (iceCream) => {
-  let sweetnessValue = iceCream.numOfSprinkles;
-  if (iceCream.flavor === "Vanilla" || iceCream.flavor === "ChocolateChip") {
-    sweetnessValue += 5;
-  } else if (
-    iceCream.flavor === "Strawberry" ||
-    iceCream.flavor === "Chocolate"
-  ) {
-    sweetnessValue += 10;
-  } else {
-    console.log("This is not a valid flavor");
-  }
+let s1 = new Student("Peter", "Waltz", "-");
 
-  return sweetnessValue;
-};
+t1.gradeStudent(s1, "VG");
+console.log(s1);
+console.log(t1);
 
-let getSweetest = (arr) => {
-  let highestSweetnessValue = 0;
+let st = new SubstiteTeacher("Emma", "Gren", ["Frontend"]);
 
-  arr.forEach((iceCream) => {
-    let sweetness = calculateSweetness(iceCream);
-    if (sweetness > highestSweetnessValue) {
-      highestSweetnessValue = sweetness;
-    }
-  });
-
-  return highestSweetnessValue;
-};
-
-console.log(getSweetest([chocoIceCream, vanillaIceCream, strawberryIceCream]));
+st.greeting();
